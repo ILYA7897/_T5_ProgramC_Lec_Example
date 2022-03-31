60e782522769c73a764239b24cf3630c174dff0f
«ILYA7897» <belobokov_ilya@mail.ru>
2022-03-31 06:15:04 +0300

    Поиск в массиве номера позиции на которой находится элемент FIND

---
+++ Example010_Method_Array/Program.cs
@@ -0,0 +1,17 @@
+﻿// Поиск в массиве номера позиции на которой находится элемент FIND(MAX)
+int [] array = {1, 12, 32, 4, 45, 23, 16, 18} ;
+
+int n = array.Length ;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index) ;
+//        break
+    }
+    index ++ ;
+}
